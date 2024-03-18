0ae452d03481ff45624892efa8e2b24cdfb17597
Aayushya Lakkadwala <the.gaming.god85@gmail.com>
2024-03-18 22:29:39 +0530

    initialised db, login, flask app

--- app.py
+++ app.py
@@ -16,3 +16,15 @@ from googleapiclient.discovery import build
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+app = Flask(__name__)
+app.secret_key = "5#v:O(TMlwMv17sPT6Hh-.b+U}80jGt~MWEJnBWg8a/~.7QdXWO%`P5cYgCk-ci"
+
+window = webview.create_window("Quad Binary Wealth AI", app)
+
+login_manager = LoginManager()
+login_required.init_app(app)
+
+cred = credentials.Certificate(os.path.join(os.path.dirname(__file__),'db.json'))
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+

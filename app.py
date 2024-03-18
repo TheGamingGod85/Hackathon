ad5049069a45cdc750d0d9c8f5b7e441aefae240
Aayushya Lakkadwala <the.gaming.god85@gmail.com>
2024-03-18 22:22:29 +0530

    Added Imports to app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+import base64
+import re
+import webview
+import pyotp
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+import firebase_admin
+from firebase_admin import credentials, firestore
+from datetime import datetime, timedelta
+import google.generativeai as genai
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+

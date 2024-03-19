d97670ed04186e3eaea90d51ed6f45996e1a7ac9
Aayushya Lakkadwala <the.gaming.god85@gmail.com>
2024-03-19 06:28:06 +0530

    added genai settings

--- app.py
+++ app.py
@@ -97,6 +97,33 @@ def totp_verify(otp, totp_secret):
     totp = pyotp.TOTP(totp_secret)
     return totp.verify(otp)
 
+genai.configure(api_key="AIzaSyB5b3yOq6uW3V32P5WeCEDuU-KSGP1hfbU")  # Set the API key for the Generative AI service
+generation_config = {
+  "temperature": 1,
+  "top_p": 1,
+  "top_k": 1,
+  "max_output_tokens": 4096,
+}
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_ONLY_HIGH"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_ONLY_HIGH"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_ONLY_HIGH"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_ONLY_HIGH"
+  },
+]
+model = genai.GenerativeModel(model_name="gemini-1.0-pro-001", generation_config=generation_config, safety_settings=safety_settings)
+
 @app.route('/')
 def index():
     access = prayatna_api_hit()
